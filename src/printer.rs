4c0c12591cbe326ed320f1ba13f9bc5173c7b7af
jweigelt <johannes.weigelt@stud.htwk-leipzig.de>
2021-11-12 10:41:55 +0100

    The table no longer contains double lines.

--- src/printer.rs
+++ src/printer.rs
@@ -1,10 +1,9 @@
 use crate::row::{Dimension, Row, RowCells};
 
-pub struct Printer;
+pub struct Printer<const C: usize>;
 
-impl Printer {
-    pub fn print<I, const C: usize>(&self, data: I)
-        where I: IntoIterator<Item=Row<C>> {
+impl<const C: usize> Printer<C> {
+    pub fn print<I: IntoIterator<Item=Row<C>>>(&self, data: I) {
         let row_cells: Vec<RowCells> = data.into_iter()
             .map(RowCells::from)
             .collect();
@@ -13,19 +12,20 @@ impl Printer {
             .map(|rc| rc.max_dimension)
             .fold(Dimension::default(), Dimension::max_merge);
 
-        self.print_cells(row_cells, max_dimension, C)
+        self.print_cells(row_cells, max_dimension)
     }
 
-    fn print_cells(&self, row_cells: Vec<RowCells>, max_cell_dimension: Dimension, columns: usize) {
-        let printed_row_dimension = Self::calculate_printed_row_dimension(max_cell_dimension, columns);
+    fn print_cells(&self, row_cells: Vec<RowCells>, max_cell_dimension: Dimension) {
+        let printed_row_dimension = Self::calculate_printed_row_dimension(max_cell_dimension);
 
+        Self::print_full_line(printed_row_dimension.width);
         for rc in row_cells {
             Self::print_row(rc, printed_row_dimension, max_cell_dimension.width)
         }
     }
 
-    fn calculate_printed_row_dimension(max_cell_dimension: Dimension, columns: usize) -> Dimension {
-        let width = (max_cell_dimension.width + 2) * columns + columns + 1;
+    fn calculate_printed_row_dimension(max_cell_dimension: Dimension) -> Dimension {
+        let width = (max_cell_dimension.width + 2) * C + C + 1;
         let height = max_cell_dimension.height + 4;
         Dimension { width, height }
     }
@@ -33,7 +33,8 @@ impl Printer {
     fn print_row(mut row_cells: RowCells, printed_row_dimension: Dimension, cell_width: usize) {
         for i in 0..printed_row_dimension.height {
             match i {
-                i if i == 0 || i == printed_row_dimension.height - 1 => Self::print_full_line(printed_row_dimension.width),
+                i if i == 0 => (),
+                i if i == printed_row_dimension.height - 1 => Self::print_full_line(printed_row_dimension.width),
                 i if i == 1 || i == printed_row_dimension.height - 2 => Self::print_blank_line(printed_row_dimension.width, cell_width),
                 _ => Self::print_cell_values(row_cells.next_values(), cell_width)
             }
